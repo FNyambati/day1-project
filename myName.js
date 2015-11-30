7426110b60cb878a1f62be01741d814a9d2ef504
FNyambati <fmnyambati@gmail.com>
2015-11-30 16:50:04 -0700

    blackjack

--- myName.js
+++ myName.js
@@ -1,2 +1,27 @@
-asdfasdf
-gersgdshnrtkehg,jhgjthdrsj,hgjhrhsjreyhtjddasdfasdf
+// Our deal function will return a random card
+var deal = function() {
+  card = Math.floor(Math.random()*52+1);
+  return card;
+};
+
+// Deal out our first hand
+var card1 = deal();
+var card2 = deal();
+
+// This function takes a card as a parameter and returns the value
+// of that card
+var getValue = function(card) {
+  // if its a face card, number should be set to 10        
+    
+  // What if it's an ace?
+
+  // Otherwise, we just want its number value
+}
+        
+// Score the hand
+function score() {
+  return getValue(card1) + getValue(card2);
+}
+
+console.log("You have cards " + card1 + " and " + card2 +
+        " for a score of " + score(card1, card2));
